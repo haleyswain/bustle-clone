810f18598fa69925ce86d20b11a49508c060f757
Haley Swain and Marty Kovach <haley@>
2017-01-10 11:53:43 -0800

    create individual story pages. move update and delete actions to story detail pages

--- app/components/story-tile.js
+++ app/components/story-tile.js
@@ -2,14 +2,4 @@ import Ember from 'ember';
 
 export default Ember.Component.extend({
   updateStoryForm: false,
-  actions: {
-    update(story, params) {
-      this.sendAction('update', story, params);
-    },
-    delete(story) {
-      if (confirm("Are you sure you want to delete this story?")) {
-        this.sendAction('destroyStory', story);
-      }
-    }
-  }
 });
--- app/routes/index.js
+++ app/routes/index.js
@@ -2,25 +2,12 @@ import Ember from 'ember';
 
 export default Ember.Route.extend({
   model() {
-    return this.store.findAll('story')
+    return this.store.findAll('story');
   },
   actions: {
     saveStory3(params) {
       var newStory = this.store.createRecord('story', params);
       newStory.save();
-      this.transitionTo('index');
-    },
-    update(story, params) {
-      Object.keys(params).forEach(function(key){
-        if(params[key]!==undefined) {
-          story.set(key,params[key]);
-        }
-      });
-      story.save();
-      this.transitionTo('index');
-    },
-    destroyStory(story) {
-      story.destroyRecord();
       this.transitionTo('index');
     }
   }
