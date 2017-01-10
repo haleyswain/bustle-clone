172ace768a3aff427a44608d2db13fe028e644d2
Haley Swain and Marty Kovach <haley@>
2017-01-10 09:34:41 -0800

    add delete functionality to stories on the index page.

--- app/routes/index.js
+++ app/routes/index.js
@@ -3,5 +3,11 @@ import Ember from 'ember';
 export default Ember.Route.extend({
   model() {
     return this.store.findAll('story')
+  },
+  actions: {
+    destroyStory(story) {
+      story.destroyRecord();
+      this.transitionTo('index');
+    }
   }
 });
